32eb341e421204df816efd3fe3c06996d8a36565
jhansilearners <125589719+jhansilearners@users.noreply.github.com>
2023-02-19 09:02:32 +0530

    Update SerializationParentChild.java
    why serialization required

--- practice-all-type-question/src/main/java/com/app/google/interviewQ/SerializationParentChild.java
+++ practice-all-type-question/src/main/java/com/app/google/interviewQ/SerializationParentChild.java
@@ -1,3 +1,25 @@
+
+
+// Let's define serialization first, then we can talk about why it's so useful.
+// 
+// Serialization is simply turning an existing object into a byte array. This byte array represents the class of the object, the version of the object, and the internal state of the object. This byte array can then be used between JVM's running the same code to transmit/read the object.
+// 
+// Why would we want to do this?
+// 
+// There are several reasons:
+// 
+// Communication: If you have two machines that are running the same code, and they need to communicate, an easy way is for one machine to build an object with information that it would like to transmit, and then serialize that object to the other machine. It's not the best method for communication, but it gets the job done.
+// 
+// Persistence: If you want to store the state of a particular operation in a database, it can be easily serialized to a byte array, and stored in the database for later retrieval.
+// 
+// Deep Copy: If you need an exact replica of an Object, and don't want to go to the trouble of writing your own specialized clone() class, simply serializing the object to a byte array, and then de-serializing it to another object achieves this goal.
+// 
+// Caching: Really just an application of the above, but sometimes an object takes 10 minutes to build, but would only take 10 seconds to de-serialize. So, rather than hold onto the giant object in memory, just cache it out to a file via serialization, and read it in later when it's needed.
+// 
+// Cross JVM Synchronization: Serialization works across different JVMs that may be running on different architectures.
+
+
+
 package com.app.google.interviewQ;
 
 import java.io.Serializable;
